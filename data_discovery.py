98d0a9ffe7b40b9cf32f9bc125ae370d86911486
Mark <mbast.amin97@gmail.com>
2020-01-07 14:24:13 -0500

    removed venv from remote repo

--- data_discovery.py
+++ data_discovery.py
@@ -2,8 +2,6 @@ import pandas as pd
 
 train_data = pd.read_csv("data/train.csv")
 
-print(train_data.info())
-print(train_data.head(5))
 
 def get_top_tweets(n):
 
---
+++ data_processing.py
@@ -0,0 +1,6 @@
+from nltk.corpus import stopwords
+import nltk
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+nltk.download('stopwords')
+stopwords = stopwords.words('english')
